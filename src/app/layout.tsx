2f331aa33c7ad8670f242874f0d749006bf25642
dawidmarek95x <dawid.marek95x@gmail.com>
2023-03-28 13:01:38 +0200

    bugfix/vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,6 @@ import { workSans } from '@/helpers/fonts';
 import Header from '@/components/Header/Header';
 import './globals.scss';
 
-export const metadata = {
-  viewport: {
-    width: 'device-width',
-    initialScale: 1,
-  },
-};
-
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
